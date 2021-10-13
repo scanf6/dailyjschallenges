43144da70aea1e54653d0196778f6450d5f84c7a
scanf6 <scanf555@gmail.com>
2021-10-13 23:10:44 +0100

    Challenge 03

---
+++ challenge03.js
@@ -0,0 +1,37 @@
+/*
+==================== CHALLENGE 03 ==================================
+Date: 13.10.2021
+
+Instructions:
+Write a function named first_non_repeating_letter that takes a string input, and returns the first character that is not repeated anywhere in the string.
+
+For example, if given the input 'stress', the function should return 't', since the letter t only occurs once in the string, and occurs first in the string.
+
+As an added challenge, upper- and lowercase letters are considered the same character, but the function should return the correct case for the initial letter. For example, the input 'sTreSS' should return 'T'.
+
+If a string contains all repeating characters, it should return an empty string ("") or None -- see sample tests.
+*/
+
+// ================ MY SOLUTION================================//
+
+function firstNonRepeatingLetter(string){
+    string = string.toLowerCase();
+    let repeatitionLetter = null;
+    string.split("").forEach(letter => {
+        let repetitionsCount = 0;
+
+        if(!repeatitionLetter) {
+            for(let i=0; i < string.length; i++) {
+                if(string[i] === letter) repetitionsCount += 1;
+            }
+        }
+
+        if(repetitionsCount === 1) repeatitionLetter = letter;
+    });
+    return repeatitionLetter ? repeatitionLetter : "";
+}
+
+// ========================= CLEVER SOLUTION ========================//
+function firstNonRepeatingLetter_clever(s) {
+    return s[s.toLowerCase().split('').findIndex(letter => s.toLowerCase().split('').filter(l => l === letter).length === 1 )] || '';
+}
